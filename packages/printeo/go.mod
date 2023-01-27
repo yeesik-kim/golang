5770286469268252fa55e270dfd2047ebc51b12a
yeesik.kim <yeesik.kim@nhn.com>
2023-01-27 18:01:10 +0900

    add package printeo

---
+++ packages/printeo/go.mod
@@ -0,0 +1,3 @@
+module github.com/yeesik-kim/golang/packages/printeo
+
+go 1.19
---
+++ packages/printeo/printeo.go
@@ -0,0 +1,8 @@
+package printeo
+
+import "fmt"
+
+func PrintlnMyView(value string) {
+	fmt.Println("my printeo start")
+	fmt.Println("value is " + value)
+}
